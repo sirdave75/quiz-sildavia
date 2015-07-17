e9d61e0836974a30c351c84e7861ed45d78bae1a
sirdave75 <david.romanillos@sildaviaweb.com>
2015-07-17 21:13:39 +0200

    despligue en heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+var creditosController = require('../controllers/creditos_controller');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question',quizController.question);
+
+router.get('/quizes/answer',quizController.answer);
+
+router.get('/creditos/author',creditosController.author);
 
 module.exports = router;
