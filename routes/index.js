517c5caac4507daf3ba4a4880d380204a31bc16b
sirdave75 <david.romanillos@sildaviaweb.com>
2015-07-23 12:03:39 +0200

    busquedas

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var creditosController = require('../controllers/creditos_controller');
 
-/* GET home page. */
+//Página de entrada (home page)
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+//Autoload de comandos ;quizId
+router.param('quizId',quizController.load);//autoload :quizId
+
+//Definición de rutas de /quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
